b19b3d9ffe20a86d144d1b3fe5148c36b4b9ed3a
Rob Merrell <robmerrell@i.tv>
2012-11-13 22:44:18 -0700

    Added a super simple object pool

---
+++ Cell Division/Assets/System/ObjectPool.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObjectPool : MonoBehaviour {
+
+	// Pool of game objects
+	GameObject[] pool = null;
+
+	// How many objects should be pooled
+	public int poolAmount = 0;
+
+	// Prefab to generate for the pool
+	public GameObject poolPrefab;
+
+
+	// Use this for initialization
+	void Start() {
+		pool = new GameObject[poolAmount];	
+
+		// create the pooled objects
+		for (int i = 0; i < poolAmount; i++) {
+			GameObject newobj = Instantiate(poolPrefab) as GameObject;
+			newobj.SetActiveRecursively(false);
+			pool[i] = newobj;
+		}
+	}
+
+
+	GameObject Activate() {
+		// find an inactive object
+		for (int i = 0; i < poolAmount; i++) {
+			if (!pool[i].active) {
+				pool[i].SetActiveRecursively(true);
+				return pool[i];
+			}
+		}
+
+		return null;
+	}
+}
